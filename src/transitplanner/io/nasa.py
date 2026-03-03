6bdf66f648b1b43e20ac5d18c22581607a30ab41
Salmanul-Farisi-551 <FarisiS@cardiff.ac.uk>
2026-03-03 23:18:22 +0000

    Update nasa.py
    performance checking

--- src/transitplanner/io/nasa.py
+++ src/transitplanner/io/nasa.py
@@ -1,23 +1,7 @@
-import pandas as pd
-from importlib import resources
+from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 
 def load_nasa_data():
-    """
-    Instantly loads the NASA data bundled in the package data folder.
-    """
-    try:
-        # This finds the file inside your src/transitplanner/data folder
-        # even after the package is installed on another computer.
-        data_path = resources.files('transitplanner.data').joinpath('nasa_data.parquet')
-        
-        with data_path.open("rb") as f:
-            df = pd.read_parquet(f)
-            
-        # Ensure 'pl_name' is the index as your previous code expected
-        if "pl_name" in df.columns:
-            df.set_index("pl_name", inplace=True)
-            
-        return df
-    except Exception as e:
-        print(f"Critical Error: Could not load bundled NASA data. {e}")
-        return None
+    table = NasaExoplanetArchive.query_criteria("pscomppars")
+    df = table.to_pandas()
+    df.set_index('pl_name', inplace=True)
+    return df
